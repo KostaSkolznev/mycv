88e8c12f01902203fdaf4cbd1a9d11147a4ee54c
kosta skolznev <k.skolzniev@gmail.com>
2019-11-04 02:05:10 +0500

    project starting

---
+++ resources/views/cv.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Curriculum Vitae - Kosta Skolznev</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    </head>
+    <body>
+      <h1>Kosta Skolznev</h1>
+    </body>
+</html>
