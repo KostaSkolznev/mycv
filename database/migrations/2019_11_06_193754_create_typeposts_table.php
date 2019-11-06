06dc59f2e4ac85bd080d5891dc5c6fdaab0c1ed1
kosta skolznev <k.skolzniev@gmail.com>
2019-11-07 00:50:40 +0500

    added table typeposts

---
+++ database/migrations/2019_11_06_193754_create_typeposts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTypepostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('typeposts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('typepost');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('typeposts');
+    }
+}
